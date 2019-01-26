05b6bc3b5c28bf73d887bffa43650aff50e845f0
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-26 02:31:31 -0500

    Added some styling and text manipulation

--- static/assets/js/text.js
+++ static/assets/js/text.js
@@ -53,4 +53,58 @@ $(function() {
         callback: function() {},
         type: 'char'
     });
+    $(".home-tlt-h1").textillate({
+        loop: false,
+        minDisplayTime: 2000,
+        initialDelay: 0,
+        inEffects: [],
+        outEffects: ['bounceOutUp'],
+        in: {
+            effect: 'flip',
+            delayScale: 1.5,
+            delay: 50,
+            sync: false,
+            shuffle: false,
+            reverse: false,
+            callback: function() {}
+        },
+        out: {
+            effect: 'bounceOutUp',
+            delayScale: 1.5,
+            delay: 50,
+            sync: true,
+            shuffle: false,
+            reverse: false,
+            callback: function() {}
+        },
+        callback: function() {},
+        type: 'char'
+    });
+    $(".home-tlt-p").textillate({
+        loop: false,
+        minDisplayTime: 2000,
+        initialDelay: 0,
+        inEffects: [],
+        outEffects: ['bounceOutDown'],
+        in: {
+            effect: 'flip',
+            delayScale: 1.5,
+            delay: 50,
+            sync: false,
+            shuffle: false,
+            reverse: false,
+            callback: function() {}
+        },
+        out: {
+            effect: 'bounceOutDown',
+            delayScale: 1.5,
+            delay: 100,
+            sync: true,
+            shuffle: false,
+            reverse: false,
+            callback: function() {}
+        },
+        callback: function() {},
+        type: 'char'
+    });
 });
